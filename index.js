e4095c7fc0bcda9797c11469903904d652537c20
jguz17 <johnathanguzman17@gmail.com>
2020-05-16 15:30:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    // WHY DOES RETURN NEED TO BE HERE !!! t-.-t
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error){
+            document.body.innerHTML = error.message
+        })
+}
